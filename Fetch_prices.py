6b3ad09aad65cf817d3378fec78f97dbc4bf9663
Allendecodes <allenjoby29@gmail.com>
2026-08-23 21:36:56 +0530

    Initial setup: API fetch + data cleaning working

---
+++ Fetch_prices.py
@@ -0,0 +1,15 @@
+import requests
+
+url = "https://api.coingecko.com/api/v3/simple/price"
+params = {
+    "ids": "bitcoin,ethereum,solana",
+    "vs_currencies": "usd",
+    "include_market_cap": "true",
+    "include_24hr_vol": "true",
+    "include_24hr_change": "true",
+    "include_last_updated_at": "true"
+}
+
+response = requests.get(url, params=params)
+print(response.status_code)
+print(response.json())
